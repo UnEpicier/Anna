ea8d7e9f9f9786f9f0393668e5ed0a5aef44ef33
Un Épicier <alexisvasseur96@gmail.com>
2026-03-31 18:54:08 +0200

    feat(app-types): add emoji and shortDescription to ServiceSchema

--- packages/app-types/src/service.ts
+++ packages/app-types/src/service.ts
@@ -4,6 +4,8 @@ export type Service = z.infer<typeof ServiceSchema>;
 export const ServiceSchema = z.object({
 	id: z.number(),
 	title: z.string(),
+	emoji: z.string(),
+	shortDescription: z.string(),
 	price: z.number(),
 	duration: z.string(),
 	description: z.string(),
